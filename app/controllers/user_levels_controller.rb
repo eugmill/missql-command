be0458651793caf0ad85c3a78faf2ec4f820a558
Sushanth Bhaskarabhatla <bhaskaraspb@gmail.com>
2014-04-09 14:25:07 -0400

    small changes

--- app/controllers/user_levels_controller.rb
+++ app/controllers/user_levels_controller.rb
@@ -5,14 +5,19 @@ class UserLevelsController < ApplicationController
     @sql_command = params[:sql_command]
     @result = ActiveRecord::Base.connection.execute(@sql_command)
     @values = [] 
+    
     @result.each do |x|
       @values << x
     end
+
     @string_values = "" 
+    
     @values[0].each do |k,v|
       @string_values << k + " | "
     end
+    
     @string_values << "\n"
+    
     @values.each do |hash|
       hash.each do |k,v|
         @string_values << v + " | "
