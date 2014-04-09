a70292bf18baf5c854f1ca2ed21cc2aee684f2cc
Sushanth Bhaskarabhatla <bhaskaraspb@gmail.com>
2014-04-09 13:35:11 -0400

    wrote code in user_levels controller to take sql input, execute, and return the result to the view

--- app/controllers/user_levels_controller.rb
+++ app/controllers/user_levels_controller.rb
@@ -3,6 +3,18 @@ class UserLevelsController < ApplicationController
   def execute
     @user_level = UserLevel.find(params[:id])
     @sql_command = params[:sql_command]
+    @result = ActiveRecord::Base.connection.execute(@sql_command)
+    @values = [] 
+    @result.each do |x|
+      @values << x
+    end
+    @string_values = "" 
+    @values.each do |hash|
+      hash.each do |k,v|
+        @string_values << k + "\n"
+        @string_values << v + "\n"
+      end
+    end
 
     respond_to do |format|
       format.js {
