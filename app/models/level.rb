c5d905a613d04da503a4c61a3fb5c08813e60d85
Sushanth Bhaskarabhatla <bhaskaraspb@gmail.com>
2014-04-09 09:57:23 -0400

    small updates to level.rb

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,4 +1,12 @@
 class Level < ActiveRecord::Base
   has_many :master_tables  
 
+  def clone_for_user(@user)
+    # checks to see if user has these tables
+    # if they dont then it users the user id and the level's master tables
+    # to generate new tables
+    # instantiates user tables
+    # then triggers postgres to magically creates the actual tables
+  end
+
 end
