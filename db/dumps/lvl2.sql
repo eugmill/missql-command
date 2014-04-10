944183d35fa16f9664899da8b284c26caedcbbb6
Eugene Millerman <e.v.millerman@gmail.com>
2014-04-10 11:08:30 -0400

    Add second level dump

---
+++ db/dumps/lvl2.sql
@@ -0,0 +1,15 @@
+CREATE TABLE cities (name VARCHAR(255), population INTEGER);
+
+INSERT INTO cities VALUES ('New York', 9000000);
+INSERT INTO cities VALUES ('Paris', 8000000);
+INSERT INTO cities VALUES ('San Francisco', 825000);
+INSERT INTO cities VALUES ('Moscow', 11000000);
+INSERT INTO cities VALUES ('Tokyo', 13000000);
+
+CREATE TABLE missiles (name VARCHAR(255), megatons INTEGER);
+
+INSERT INTO cities VALUES ('Big Boy', 34000000);
+INSERT INTO cities VALUES ('Fat Man', 54500000);
+INSERT INTO cities VALUES ('Smiling Buddha', 213450);
+INSERT INTO cities VALUES ('Tsar Bomba', 1134000);
+INSERT INTO cities VALUES ('Trinity', 11400);
