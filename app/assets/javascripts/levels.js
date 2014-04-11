2b0cc9117d9550bdcc95f3ac8a1fa6148542eb14
Sushanth Bhaskarabhatla <bhaskaraspb@gmail.com>
2014-04-11 15:27:19 -0400

    completed ajax loop for repl. bind execute button to retrieve json for repl.

--- app/assets/javascripts/levels.js
+++ app/assets/javascripts/levels.js
@@ -1,13 +1,33 @@
-$(document).ready(function() {
-
 flashNotice = function(text){
-	var container = $('div#flash div#notice');
-	return container.html(text)
+  var container = $('div#flash div#notice');
+  return container.html(text)
 }
 
 appendToOutput = function(text) {
   var container = $('div#output pre');
   return container.html(text);
 }
+
+submitQuery = function(text) {
+  $.post('/query', 
+        {sql_command: text},
+        updateView,
+        "json"
+    )
+}
+
+updateView = function(data){
+  alert(data);
+  console.log(data);
+}
+
+$(document).ready(function() {
+
+  $('#execute').on("click",function(){
+      var text = $('#sql-command').val();
+      return submitQuery(text);
+    }
+
+    )
 
 })
