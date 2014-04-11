3403b96f76a749fa6f2f5b8a3b72770dda8bf2d7
Eugene Millerman <e.v.millerman@gmail.com>
2014-04-11 13:31:10 -0400

    Remove unnecessary user_level variable

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -4,7 +4,6 @@ class LevelsController < ApplicationController
 
   def show
     @level = Level.find_by(:stage_number => params[:id])
-    @user_level = UserLevel.find_or_create_by(:user_id => current_user.id, :level_id => @level.id)
     if @level != current_user.current_level
       current_user.user_database.load_level(@level)
     end
