942f0c27df6fc3cc53b6c9d3909eab5759f886da
Eugene Millerman <e.v.millerman@gmail.com>
2014-04-11 11:16:17 -0400

    Initialize user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe User do
-    
+  it "should have a user database after creation" do
+    pending
+  end
+
+  it "should return its current level" do
+    pending
+  end    
 end
