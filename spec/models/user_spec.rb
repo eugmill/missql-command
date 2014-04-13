7c55db538ea176a8cb243cfdda05c9de9b84b466
Eugene Millerman <e.v.millerman@gmail.com>
2014-04-12 21:37:45 -0400

    Add user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,16 @@
 require 'spec_helper'
 
 describe User do
+  before :each do 
+    @user = FactoryGirl.create(:user)
+  end 
+
   it "should have a user database after creation" do
-    pending
+    expect(@user.user_database).not_to be_nil
   end
 
   it "should return its current level" do
-    pending
-  end    
+    expect(@user.current_level).to eq(@user.user_database.level)
+  end
+
 end
