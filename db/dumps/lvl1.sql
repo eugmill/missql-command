9476140c2f1e0e1d196877c305ffce862f131fb3
Eugene Millerman <e.v.millerman@gmail.com>
2014-04-14 11:46:26 -0400

    Update level copy

--- db/dumps/lvl1.sql
+++ db/dumps/lvl1.sql
@@ -1,7 +1,9 @@
 CREATE TABLE cities (id serial PRIMARY KEY,name VARCHAR(255), population INTEGER);
 
-INSERT INTO cities (name,population) VALUES ('New York', 9000000);
-INSERT INTO cities (name,population) VALUES ('Paris', 8000000);
+INSERT INTO cities (name,population) VALUES ('New York', 8337000);
+INSERT INTO cities (name,population) VALUES ('Paris', 2211000);
 INSERT INTO cities (name,population) VALUES ('San Francisco', 825000);
-INSERT INTO cities (name,population) VALUES ('Moscow', 11000000);
-INSERT INTO cities (name,population) VALUES ('Tokyo', 13000000);
+INSERT INTO cities (name,population) VALUES ('London', 8308000);
+INSERT INTO cities (name,population) VALUES ('Moscow', 11500000);
+INSERT INTO cities (name,population) VALUES ('Tokyo', 13230000);
+INSERT INTO cities (name,population) VALUES ('Shanghai', 24000000);
