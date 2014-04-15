dc8f3cea9888eb87724e7e56f726764e9a7b73fc
ErikPeterson <eriksalgstrom@gmail.com>
2014-04-15 11:16:18 -0400

    Level 8 first draft.

---
+++ db/dumps/lvl8.sql
@@ -0,0 +1,10 @@
+CREATE TABLE missiles (id serial PRIMARY KEY, model_name VARCHAR(255), nationality VARCHAR(255), target_city INTEGER);
+
+INSERT INTO missiles (model_name, nationality, target_city) VALUES ('Peacekeeper','USA', 7);
+INSERT INTO missiles (model_name, nationality, target_city) VALUES ('Minuteman','USA', 5);
+INSERT INTO missiles (model_name, nationality, target_city) VALUES ('Topol','Russia', 1);
+INSERT INTO missiles (model_name, nationality, target_city) VALUES ('Yars','Russia', 4);
+INSERT INTO missiles (model_name, nationality, target_city) VALUES ('Jericho III','Israel', 5);
+INSERT INTO missiles (model_name, nationality, target_city) VALUES ('Agni-V','India', 7);
+INSERT INTO missiles (model_name, nationality, target_city) VALUES ('Dongfeng-5','China', 3);
+INSERT INTO missiles (model_name, nationality, target_city) VALUES ('Dongfeng-31','China', 6);
