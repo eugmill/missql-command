026ff5955fecd5e5eebf1f865ecd541b8384ebd4
Eugene Millerman <e.v.millerman@gmail.com>
2014-04-16 14:41:40 -0400

    Clean up old cold

--- app/controllers/gameplay_controller.rb
+++ app/controllers/gameplay_controller.rb
@@ -1,27 +1,6 @@
 class GameplayController < ApplicationController
-
   def execute
     @response = GameResponse.new(current_user, params[:sql_command]).json
-
-    # @output = current_user.user_database.execute(@sql_command)
-    
-    # @values = @response[""][:result]
-    # @notice = @response.correct?.to_s
-
-    # @string_values = "" 
-    
-    # @values[0].each do |k,v|
-    #   @string_values << k + " | "
-    # end
-    
-    # @string_values << "\n"
-    
-    # @values.each do |hash|
-    #   hash.each do |k,v|
-    #     @string_values << v + " | "
-    #   end
-    # @string_values << "\n"      
-    # end
 
     respond_to do |format|
       format.json {
@@ -29,5 +8,4 @@ class GameplayController < ApplicationController
       }
     end
   end
-
 end
