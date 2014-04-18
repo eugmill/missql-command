00b0975613b7f186aba655b1907b27e1c11a387d
ErikPeterson <eriksalgstrom@gmail.com>
2014-04-18 16:00:20 -0400

    Add more test methods.

--- app/models/level_test.rb
+++ app/models/level_test.rb
@@ -9,8 +9,20 @@ def has_col(expected, tuples)
     tuples.all?{|t| t.keys.include?(expected)}
 end
 
+def has_only_cols(expected, tuples)
+	tuples.all?{|t| t.keys - %w(expected) == 0}
+end
+
+def has_only_col(expected, tuples)
+    tuples.all?{|t| t.keys.length == 1 && t.keys.include?(expected)}
+end
+
 def has_row(expected, tuples)
 	tuples.include?(eval(expected))
+end
+
+def has_only_row(expected, tuples)
+	tuples.length == 1 && has_row(expected, tuples)
 end
 
 def is_record(expected, tuples)
