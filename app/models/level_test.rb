84522308f8a7d96137455f5928d54795b1e6a125
ErikPeterson <eriksalgstrom@gmail.com>
2014-04-18 16:42:46 -0400

    Refactor testing methods

--- app/models/level_test.rb
+++ app/models/level_test.rb
@@ -1,35 +1,24 @@
 class LevelTest < ActiveRecord::Base
   belongs_to :level
 
-def test_pg(pgresult)
+def pg_correct?(pgresult)
 	self.send(self.test_query, self.expected_output, pgresult.to_a)
 end
 
-def has_col(expected, tuples)
+def has_col?(expected, tuples)
     tuples.all?{|t| t.keys.include?(expected)}
 end
 
-def has_only_cols(expected, tuples)
-	tuples.all?{|t| t.keys - %w(expected) == 0}
+def has_col_count?(expected, tuples)
+	return false if tuples.empty?
+	tuples[0].keys.count == expected.to_i
 end
 
-def has_only_col(expected, tuples)
-    tuples.all?{|t| t.keys.length == 1 && t.keys.include?(expected)}
-end
-
-def has_row(expected, tuples)
+def has_row?(expected, tuples)
 	tuples.include?(eval(expected))
 end
 
-def has_only_row(expected, tuples)
-	tuples.length == 1 && has_row(expected, tuples)
-end
-
-def is_record(expected, tuples)
-	tuples[0] == eval(expected)
-end
-
-def has_enough_rows(expected, tuples)
+def has_row_count?(expected, tuples)
 	tuples.count == expected.to_i
 end
 
