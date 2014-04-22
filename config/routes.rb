89cd4426cd358955ab82d444641b5e2208775cef
ErikPeterson <eriksalgstrom@gmail.com>
2014-04-22 12:50:31 -0400

    Move about to pages controller.

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   post 'users' => 'users#create'
   get 'users/:id' => 'users#show', as: :user
 
-  get '/about' => 'users#about', as: :about
+  get '/about' => 'pages#about', as: :about
 
   get 'login' => 'sessions#new'
   get 'logout' => 'sessions#destroy'
