e8b2ce40f61e9e6f843f53d67a29d31131a83c4a
Sushanth Bhaskarabhatla <bhaskaraspb@gmail.com>
2014-04-22 13:04:22 -0400

    added sush's about text. cleaned up pages controller.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,7 @@ class PagesController < ApplicationController
   before_action :set_levels
 
   def index
-  	return redirect_to "/about" if !current_user || current_user.guest?
-
-  	if current_user.current_level
-  		return redirect_to "/levels/#{current_user.current_level.stage_number}"
-  	end
+  	render :about
   end
 
   def about
