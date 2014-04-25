98422227012d1ab7927c2a66100eda241d0a1557
ErikPeterson <eriksalgstrom@gmail.com>
2014-04-24 20:37:57 -0400

    Refactor level three to use city_name instead of name.

--- db/dumps/lvl3.sql
+++ db/dumps/lvl3.sql
@@ -1,9 +1,9 @@
-CREATE TABLE cities (name VARCHAR(255), population INTEGER);
+CREATE TABLE cities (city_name VARCHAR(255), population INTEGER);
 
-INSERT INTO cities (name,population) VALUES ('New York', 8337000);
-INSERT INTO cities (name,population) VALUES ('Paris', 2211000);
-INSERT INTO cities (name,population) VALUES ('San Francisco', 825000);
-INSERT INTO cities (name,population) VALUES ('London', 8308000);
-INSERT INTO cities (name,population) VALUES ('Moscow', 11500000);
-INSERT INTO cities (name,population) VALUES ('Tokyo', 13230000);
-INSERT INTO cities (name,population) VALUES ('Shanghai', 24000000);
+INSERT INTO cities (city_name, population) VALUES ('New York', 8337000);
+INSERT INTO cities (city_name, population) VALUES ('Paris', 2211000);
+INSERT INTO cities (city_name, population) VALUES ('San Francisco', 825000);
+INSERT INTO cities (city_name, population) VALUES ('London', 8308000);
+INSERT INTO cities (city_name, population) VALUES ('Moscow', 11500000);
+INSERT INTO cities (city_name, population) VALUES ('Tokyo', 13230000);
+INSERT INTO cities (city_name, population) VALUES ('Shanghai', 24000000);
